9f7b649f8de2fcbca21b5cc5daac9440cdf7bc27
AntoCic <anto.cic.127@gmail.com>
2024-06-05 16:36:01 +0200

    ADD: model type with migration and seeder

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('types')->truncate();
+        $types = ['HTML', 'CSS','JavaScript','Bootstrap', 'Node', 'VUE', 'Vite', 'PHP', 'laravel'];
+        foreach ($types as $type) {
+            $new_type = new Type();
+            $new_type->name = $type;
+            $new_type->slug = Str::slug($type);
+
+            $new_type->save();
+        }
+    }
+}
